689268375cb5cc4deba0aea442a0ccaafe9ffbda
cgp <gjelstrup78@gmail.com>
2026-02-05 19:27:57 +0100

    Adjust background image width in BackgroundImage component for improved layout consistency.

--- components/background-image.tsx
+++ components/background-image.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const BackgroundImage = () => {
   return (
     <div
-      className='fixed left-1/2 z-[-10] h-[700px] min-h-[700px] w-[666px] -translate-x-[calc(50%+2rem)] sm:-left-8 sm:translate-x-0 sm:h-[80svh] sm:w-[calc(100%+50px)]'
+      className='fixed left-1/2 z-[-10] h-[700px] min-h-[700px] w-[600px] -translate-x-[calc(50%+2rem)] sm:-left-8 sm:translate-x-0 sm:h-[80svh] sm:w-[calc(100%+50px)]'
       id='bg-image'
       aria-hidden
     >
